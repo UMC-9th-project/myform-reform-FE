145f9bbe9455245f1974847aa58a39acce2e5d26
gain-0525 <kimgain0525@sookmyung.ac.kr>
2025-12-31 12:50:47 +0900

    fix: UI 색상 변경

--- src/components/domain/chat/SendButton.tsx
+++ src/components/domain/chat/SendButton.tsx
@@ -14,7 +14,7 @@ const SendButton: React.FC<SendButtonProps> = ({ isActive, onClick }) => {
         w-fit px-18 py-4
         rounded-[1.25rem] shadow-sm transition-all duration-200 heading-h4-sb
         ${isActive 
-          ? 'bg-black text-white cursor-pointer hover:bg-gray-900 active:scale-[0.96]' 
+          ? 'bg-black text-white cursor-pointer active:scale-[0.96]' 
           : 'bg-[var(--color-gray-50)] text-white cursor-not-allowed'
         }
       `}
