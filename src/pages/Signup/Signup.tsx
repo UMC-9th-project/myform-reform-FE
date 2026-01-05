2a7d37f0f855d7d80d7f493d622d050b6f0397dd
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-01-06 03:01:16 +0900

    feat : 회원가입 페이지 UI 구현

---
+++ src/pages/Signup/Signup.tsx
@@ -0,0 +1,54 @@
+import Layout from '../../components/layout/PageLayout';
+import logo2 from '../../assets/logos/logo2.svg';
+import Button1 from '../../components/common/Button/button1';
+
+const KakaoLogin = () => {
+  const kakaoLoginWindow = window.open(
+    'https://kauth.kakao.com/oauth/authorize?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&response_type=code',
+    '_blank'
+  );
+  return kakaoLoginWindow;
+};
+
+const Signup = () => {
+  return (
+    <Layout showHeader={false} showNavbar={false} showFooter={false}>
+      <div className=" w-[543px] mx-auto flex flex-col justify-center items-center h-screen">
+        <div className="mb-[1.5625rem] py-[2.16688rem]">
+          <img src={logo2} alt="logo" />
+        </div>
+
+        <div className="text-center heading-h4-bd mb-[2.8125rem]">
+          <p>스포츠 유니폼 리폼 플랫폼,</p>
+          <p>내폼리폼과 함께 쉽게 리폼을 시작해요!</p>
+        </div>
+
+        <Button1
+          variant="white"
+          size="default"
+          onClick={KakaoLogin}
+          className="body-b0-md w-full mb-[1.25rem] "
+        >
+          카카오 계정으로 로그인하기
+        </Button1>
+
+        <Button1
+          variant="white"
+          size="default"
+          className="body-b0-md w-full mb-[2.5625rem] "
+        >
+          이메일로 시작하기
+        </Button1>
+
+        <div className="body-b1-rg flex py-[1.8125rem] px-[1.875rem] gap-[1.8125rem]">
+          <p>이미 내폼리폼 회원이신가요?</p>
+          <p className="text-[var(--color-mint-1)] underline cursor-pointer hover:scale-95 transition-all duration-300">
+            로그인
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Signup;
