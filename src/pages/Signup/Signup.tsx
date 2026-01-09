f6a0f3145e1621a3668e2726f3f93ed549255869
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-01-10 03:25:32 +0900

    feat : 카카오톡 아이콘 추가 & 네비게이션 추가

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,6 +1,10 @@
 import Layout from '../../components/layout/PageLayout';
-import logo2 from '../../assets/logos/logo2.svg';
 import Button1 from '../../components/common/Button/button1';
+
+import logo2 from '../../assets/logos/logo2.svg';
+import kakao from '../../assets/signup/kakao.svg';
+
+import { useNavigate } from 'react-router-dom';
 
 const KakaoLogin = () => {
   const kakaoLoginWindow = window.open(
@@ -11,6 +15,7 @@ const KakaoLogin = () => {
 };
 
 const Signup = () => {
+  const navigate = useNavigate();
   return (
     <Layout showHeader={false} showNavbar={false} showFooter={false}>
       <div className=" w-[543px] mx-auto flex flex-col justify-center items-center h-screen">
@@ -27,15 +32,19 @@ const Signup = () => {
           variant="kakao"
           size="default"
           onClick={KakaoLogin}
-          className="body-b0-md w-full mb-[1.25rem] "
+          className="body-b0-md w-full mb-[1.25rem] rounded-[0.9375rem]"
         >
+          <img src={kakao} alt="kakao" />
           카카오 계정으로 로그인하기
         </Button1>
 
         <Button1
           variant="white"
           size="default"
-          className="body-b0-md w-full mb-[2.5625rem] "
+          className="body-b0-md w-full mb-[2.5625rem] rounded-[0.9375rem]"
+          onClick={() => {
+            navigate('/signup/UserTypeSelection');
+          }}
         >
           이메일로 시작하기
         </Button1>
