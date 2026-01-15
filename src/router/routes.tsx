cf19ed37cfce2e1cdd4060577398571fc7782bdf
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-01-16 01:21:13 +0900

    feat : 라우터 초기 설정

---
+++ src/router/routes.tsx
@@ -0,0 +1,41 @@
+import Home from '../pages/Home';
+import Test from '../pages/Test';
+
+// 회원가입
+import Signup from '../pages/Signup/Signup';
+import SignupIndex from '../pages/Signup/SignupIndex';
+import UserTypeSelection from '../pages/Signup/UserTypeSelection';
+import SignupFormCustomer from '../pages/Signup/SignupFormCustomer';
+
+export const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+    children: [
+      {
+        index: true,
+        element: <SignupIndex />,
+      },
+      {
+        path: 'type',
+        element: <UserTypeSelection />,
+      },
+      {
+        path: 'form-customer',
+        element: <SignupFormCustomer />,
+      },
+    ],
+  },
+  {
+    path: '/test',
+    element: <Test />,
+  },
+  {
+    path: '*',
+    element: <div>Not Found</div>,
+  },
+];
