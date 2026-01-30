18ba641f79eeffccfcffabb296ae5ba0331ab9be
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-01-30 20:55:12 +0900

    fix : 로그인 시 타입 분류 & 로그아웃 api 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,5 @@
 import { api } from './axios';
-import type { LoginRequest, LoginResponse } from '../types/api/auth';
+import type { LoginRequest, LoginResponse, LogoutResponse } from '../types/api/auth';
 
 
 export const loginUser = async (
@@ -9,5 +9,10 @@ export const loginUser = async (
     '/api/v1/auth/login/local',  
     data
   );
+  return response.data;
+};
+
+export const logoutUser = async (): Promise<LogoutResponse> => {
+  const response = await api.post<LogoutResponse>('/auth/logout');
   return response.data;
 };
