c9261c875c6651668faad5ad252f6a50dbe28268
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-01-30 22:48:58 +0900

    feat: 판매 관리 내역 조회

--- src/api/profile/sale.ts
+++ src/api/profile/sale.ts
@@ -1,6 +1,7 @@
 import { api } from '../axios';
 import type { CreateSaleRequest } from '../../types/domain/mypage/sale';
 import type { CreateOrderRequest } from '../../types/domain/mypage/order';
+import type { orderListItem } from '../../types/domain/mypage/orderList';
 
 export const createSale = async (payload: CreateSaleRequest) => {
   const { data } = await api.post('/profile/add/item', payload);
@@ -10,4 +11,14 @@ export const createSale = async (payload: CreateSaleRequest) => {
 export const createOrder = async (payload: CreateOrderRequest) => {
   const { data } = await api.post('/profile/add/reform', payload);
   return data.success; // string
+};
+
+export const getOrders = async ({ type }: { type: 'ITEM' | 'REFORM' }): Promise<orderListItem[]> => {
+  const { data } = await api.get('/profile/sales', { params: { type } });
+
+  if (data.resultType === 'SUCCESS' && data.success) {
+    return data.success as orderListItem[];
+  } else {
+    return [];
+  }
 };
