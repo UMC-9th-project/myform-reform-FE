765043329b2aa4247adeb297335f5fbbe12ed582
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-01-30 15:16:42 +0900

    chore: input 폴더명 변경

--- src/components/domain/signup/Form/SignupForm.tsx
+++ src/components/domain/signup/Form/SignupForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Input from '../../../common/input_tmp/Input';
+import Input from '../../../common/input/Input';
 import Button from '../../../common/button/button1';
 import AgreementSection from '../AgreementSection';
 import {
