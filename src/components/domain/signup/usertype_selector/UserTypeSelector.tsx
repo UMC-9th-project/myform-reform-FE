fd02a1dc8c4743cbb8d0429a646fe8c6c5411c7d
gain-0525 <kimgain0525@sookmyung.ac.kr>
2026-02-04 19:10:30 +0900

    fix: 빌드 에러 수정

--- src/components/domain/signup/usertype_selector/UserTypeSelector.tsx
+++ src/components/domain/signup/usertype_selector/UserTypeSelector.tsx
@@ -4,7 +4,7 @@ export const TYPOGRAPHY_CLASSES = {
 } as const;
 
 export const CARDCLASSES =
-  'w-[297px] h-[321px] PX-[2.3125rem] py-[2.25rem] flex flex-col gap-[0.625rem] rounded-[1.25rem] bg-[var(--color-gray-20)] border border-[var(--color-gray-20)] hover:bg-[var(--color-white)] hover:border-[var(--color-black)] hover:shadow-[0_4px_15.4px_0_rgba(0,0,0,0.09)] transition-all duration-200 cursor-pointer' as const;
+  'w-[297px] h-[321px] px-[2.3125rem] py-[2.25rem] flex flex-col gap-[0.625rem] rounded-[1.25rem] bg-[var(--color-gray-20)] border border-[var(--color-gray-20)] hover:bg-[var(--color-white)] hover:border-[var(--color-black)] hover:shadow-[0_4px_15.4px_0_rgba(0,0,0,0.09)] transition-all duration-200 cursor-pointer' as const;
 
 interface UserTypeCardProps {
   icon: string;
